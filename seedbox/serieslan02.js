1050d7398d206b587b17eda07db71e76dffd49df
Gustavo Cabrera <gcabrera@kirios.com.ar>
2022-09-04 11:21:47 -0300

    Create serieslan02.js

---
+++ seedbox/serieslan02.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         SeriesLan 02
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://viteca.stream/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=viteca.stream
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("Use https://addons.opera.com/en/extensions/details/copy-urls-2/ to copy all tabs from Opera");
+    function openMega() {
+        debugger;
+        var iframe = document.getElementsByTagName("iframe")[0];
+        if(iframe && iframe.src && iframe.src.includes("mega.nz")) {
+            window.open(iframe.src);
+            window.close();
+        }
+    }
+    setTimeout(openMega, 5000);
+})();
